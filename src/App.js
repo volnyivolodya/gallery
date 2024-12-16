ea0d44fff1f9e6c460747e1122def91a9d5f948b
Vlagimir Ivanov <volnyivolodya@gmail.com>
2024-12-16 16:30:51 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Gallery from "./components/Gallery";
+
+function App() {
+  return (
+    <Gallery
+      images={[
+        "/images/image 1.jpg",
+        "/images/image 2.jpg",
+        "/images/image 3.jpg",
+        "/images/image 1.jpg",
+        "/images/image 2.jpg",
+        "/images/image 3.jpg",
+      ]}
+    />
+  );
+}
+
+export default App;
